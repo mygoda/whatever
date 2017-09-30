b68557f3669f98aecf2b6c3713cf211188e79677
Tao-xu <taoxu@163.com>
2017-09-30 12:05:10 +0800

    add randowm sleep

--- zhihu/models.py
+++ zhihu/models.py
@@ -16,7 +16,7 @@ class ZhihuMember(CommonModelMixin, models.Model):
     name = models.CharField(u"姓名", max_length=128)
     gender = models.CharField(u"性别", default="1", max_length=2, choices=SEX)
     user_type = models.CharField(u"用户类型", max_length=16, null=True, blank=True)
-    url_token = models.CharField(u"用户URL", max_length=32, unique=True)
+    url_token = models.CharField(u"用户URL", max_length=255, unique=True)
     is_advertiser = models.BooleanField("是否广告", default=False)
     avatar_url = models.CharField(u"头像", max_length=255, null=True, blank=True)
     is_org = models.BooleanField(u"是否组织", default=False)
